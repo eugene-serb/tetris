f2082e2dc6c7f83286be2e9daf9911324efe9eb3
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2022-12-25 16:07:52 +0300

    #1 – update html template, modify tetris styles, add styles for table, delete content styles, modified service and index css, init js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+
+const $MAP = document.querySelector('#map');
+const $SCORE = document.querySelector('#score');
+const $TIME = document.querySelector('#timer');
+const $RATING = document.querySelector('#rating');
